b04b934d15bde55e9126970f4725986379dd4bd2
Husnoro <Husnoro>
2020-04-21 20:49:03 +0500

    add signing, loging

--- interimProject/Program.cs
+++ interimProject/Program.cs
@@ -6,7 +6,17 @@ namespace interimProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Push *1* --> to Log In\nPush *2* --> to Sign Up");
+            int directory = int.Parse(Console.ReadLine());
+            switch (directory) 
+            {
+                case 1:
+                break;
+                case 2:
+                break;
+
+            }
+
         }
     }
 }
