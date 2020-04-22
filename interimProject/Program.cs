c174f2b54c65a93432caec9a4e8293da48a8dc0d
Husnoro <Husnoro>
2020-04-22 15:26:49 +0500

    divide entrance for client and almin

--- interimProject/Program.cs
+++ interimProject/Program.cs
@@ -12,23 +12,46 @@ namespace interimProject
             string AlifFamilyPassword ="29-04-2014";
             register.OpenConnection();
             //register.CheckingConnection();
+            firstMenu:
             Console.WriteLine("Push *1* --> to Log In\nPush *2* --> to Sign Up");
             int directory = int.Parse(Console.ReadLine());
             switch (directory)
             {
-                case 1: 
-                System.Console.WriteLine("Enter your phone number: ");
-                string inputLogin = Console.ReadLine();
-                System.Console.WriteLine("Enter system-password: ");
-                string inputSystemPassword = Console.ReadLine();
-                register.Checkingidentity(inputLogin, inputSystemPassword);
-
-                break;
+                case 1: // loging case
+                    lie1:
+                    System.Console.WriteLine("Are you a part of Alif-Family?\nPush *1*--> if you are an Alif-bank employee\nPush *2*--> if you are not an Alif-bank employee\nPush *3*--> to exit");
+                    int choise2 = int.Parse(Console.ReadLine());
+                    if (choise2 == 1){
+                        System.Console.WriteLine("Enter Alif-Family-password: ");
+                        string inPutAlifFamilyPassword = Console.ReadLine();
+                        if (inPutAlifFamilyPassword == AlifFamilyPassword){
+                            System.Console.WriteLine("Enter your phone number: ");
+                            string inputLogin = Console.ReadLine();
+                            System.Console.WriteLine("Enter system-password: ");
+                            string inputSystemPassword = Console.ReadLine();
+                            register.Checkingidentity(inputLogin, inputSystemPassword);
+                            goto PAMenuForAdmins;
+                            } else {
+                                Console.Clear();
+                                System.Console.WriteLine("Error password!");
+                                goto lie1;
+                            }
+                    } else if (choise2 == 2) {
+                        System.Console.WriteLine("You will be registered as a regular user!");
+                        System.Console.WriteLine("Enter your phone number: ");
+                        string inputLogin = Console.ReadLine();
+                        System.Console.WriteLine("Enter system-password: ");
+                        string inputSystemPassword = Console.ReadLine();
+                        register.Checkingidentity(inputLogin, inputSystemPassword);
+                        goto PAMenuForClient;
+                    } else {
+                        goto firstMenu;
+                    }
                 case 2:  // register case
                     lie:
                     System.Console.WriteLine("Are you a part of Alif-Family?\nPush *1*--> if you are an Alif-bank employee\nPush *2*--> if you are not an Alif-bank employee");
-                    int choise2 = int.Parse(Console.ReadLine());
-                    if (choise2 == 1){
+                    int choise3 = int.Parse(Console.ReadLine());
+                    if (choise3 == 1){
                         System.Console.WriteLine("Enter Alif-Family-password: ");
                         string inPutAlifFamilyPassword = Console.ReadLine();
                         if (inPutAlifFamilyPassword == AlifFamilyPassword){
@@ -46,7 +69,6 @@ namespace interimProject
                             System.Console.WriteLine("Create system-password:");
                             string aSystemPassword = Console.ReadLine();
                             register.AdminInsert(aLastName,aFirstName, aMiddleName, aLogin, aPassportID, aSystemPassword);
-                            goto secondMenu;
                         } else{
                             Console.Clear();
                             System.Console.WriteLine("Error password!");
@@ -68,10 +90,14 @@ namespace interimProject
                             string cSystemPassword = Console.ReadLine();
                             register.ClientInsert(cLastName,cFirstName, cMiddleName, cLogin, cPassportID, cSystemPassword);
                              }
-                    goto secondMenu;
+                    goto PAMenuForClient;
             }
-            secondMenu:
-            System.Console.WriteLine(" ");
+            PAMenuForClient:
+            System.Console.WriteLine(" hello! ");
+
+            
+            PAMenuForAdmins:
+            System.Console.WriteLine(" ghjk ");
 
 
 
