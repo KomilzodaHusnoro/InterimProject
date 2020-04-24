946624ced2746e3a5cef1b9c425a0552cd74223a
Husnoro <Husnoro>
2020-04-24 19:46:29 +0500

    add fields & class-constructor to class endMassage

--- interimProject/ConnectingToadmin.cs
+++ interimProject/ConnectingToadmin.cs
@@ -4,17 +4,32 @@ using System.Data.SqlClient;
 
 namespace interimProject
 {
-    class Sendmassage
+    class SendMassage
     {
-        public string login {get; set;}
-        public string company {get; set;}
-        public Sendmassage ()
+        public string login { get; set; }
+        public string company { get; set; }
+        public string massage {get; set; }
+        public SendMassage()
 
+        {
+            System.Console.WriteLine("*You can write your massage here*");
+            string massage = Console.ReadLine();
+        }
+        const string conS = @"Data Source= localhost; Initial Catalog = LoanCalculator; user id= sa; password=Root123.";
+        SqlConnection conForLc = new SqlConnection(conS);
+        public void massageInsert ()
+        {
+            string insertingSqlCommand = string.Format($"insert into ConnectingAdmin ([login],[Company],[massage]) values ('{login}','{company}','{massage}'");
+            if (ConnectionState.Closed == conForLc.State )
+                {conForLc.Open();}
+            SqlCommand command = new SqlCommand(insertingSqlCommand,conForLc);
+            var result = command.ExecuteNonQuery();
+            Console.Clear();
+            if (result > 0)
             {
-               System.Console.WriteLine("*You can write your massage here*");
-               string massage = Console.ReadLine(); 
+                System.Console.WriteLine($"Successfully registered!\n Wellcome!");
             }
-        
-        
+        }
+
     }
 }
